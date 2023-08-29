1e132c3190bf0b2763acc847f28da427eb8ca0a1
MemesOnMondays <143081282+MemesOnMondays@users.noreply.github.com>
2023-08-30 00:55:21 +0100

    Add files via upload

--- contracts/Airdrop.sol
+++ contracts/Airdrop.sol
@@ -1,1 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
 
+contract AirDrop {
+
+    string[] public addresses;
+    string public winner;
+    bool public isDrawn = false;
+
+    constructor(string[] memory _addresses) {
+        addresses = _addresses;
+    }
+
+    function selectWinner() public {
+        require(!isDrawn, "The winner is already selected.");
+        uint256 index = block.prevrandao % addresses.length;
+        winner = addresses[index];
+        isDrawn = true;
+    }
+
+}
